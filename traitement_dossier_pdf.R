03678438837527b62d63e563965b8226624e047d
Ameliee12 <amelie.brejot@wanadoo.fr>
2024-11-27 10:26:40 +0100

    update fonction traitement dossier

--- traitement_dossier_pdf.R
+++ traitement_dossier_pdf.R
@@ -2,9 +2,21 @@ library(dplyr)
 
 source("fonction_traitement_pdf.R")
 
-traiter_dossier_pdf <- function(dossier_pdf){   # format chemin du dossier
+traiter_dossier_pdf <- function(dossier_pdf, df_existant = NULL){   # format chemin du dossier
   # Obtenir la liste des fichiers pdf du dossier
   fichiers <- list.files(dossier_pdf, pattern = "\\.pdf$", full.names=TRUE)
+  
+  # Vérifier quels fichiers ont déjà été traités
+  if (!is.null(df_existant)) {
+    fichiers_existants <- unique(df_existant$fichier)  # Récupérer les fichiers déjà présents
+    fichiers <- fichiers[!(basename(fichiers) %in% fichiers_existants)]  # Exclure ceux déjà traités
+  }
+  
+  # Si aucun nouveau fichier, retourner le dataframe existant
+  if (length(fichiers) == 0) {
+    message("Aucun nouveau fichier à traiter.")
+    return(df_existant)
+  }
   
   resultats <- lapply(fichiers, function(fichier) {
     # Étape 1 : Traiter le PDF
@@ -12,6 +24,8 @@ traiter_dossier_pdf <- function(dossier_pdf){   # format chemin du dossier
     
     # Étape 2 : Ajouter des variables
     df_avec_variables <- ajout_variables(df_pdf)
+    
+    df_avec_variables$fichier <- basename(fichier)
     
     return(df_avec_variables)
   })
