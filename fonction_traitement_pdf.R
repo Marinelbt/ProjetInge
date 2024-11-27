f001c2ee66597bb6d8500b3781b731b1bf481dc4
Ameliee12 <amelie.brejot@wanadoo.fr>
2024-11-27 09:22:03 +0100

    Update2 fonction traitement données

--- fonction_traitement_pdf.R
+++ fonction_traitement_pdf.R
@@ -11,8 +11,6 @@ traitement_pdf <- function(pdf_file){   # format "nom_doc.pdf"
   # Diviser le contenu en lignes
   lines <- unlist(strsplit(text, "\n"))
   
-  ##### Déroulé du match #####
-  
   # Extraire les données de chaque ligne contenant un motif de temps et de score
   table_lines <- lines[str_detect(lines, "\\d{2}:\\d{2}\\s+\\d{2} - \\d{2}")]
   
@@ -20,11 +18,13 @@ traitement_pdf <- function(pdf_file){   # format "nom_doc.pdf"
   extracted_data <- lapply(table_lines, function(line) {
     matches <- str_match_all(line, "(\\d{2}:\\d{2})\\s+(\\d{2} - \\d{2})\\s+(.*?)(?=(\\d{2}:\\d{2}|$))")
     data.frame(
-      temps = str_trim(matches[[1]][, 2]),
-      score = str_trim(matches[[1]][, 3]),
-      action = str_trim(matches[[1]][, 4])
+      temps = matches[[1]][, 2],
+      score = matches[[1]][, 3],
+      action = matches[[1]][, 4]
     )
   })
+  
+  extracted_data$score <- as.numeric(extracted_data$score)
   
   # Combiner toutes les lignes extraites en un seul data frame
   df_match <- do.call(rbind, extracted_data)
@@ -38,7 +38,7 @@ traitement_pdf <- function(pdf_file){   # format "nom_doc.pdf"
   # Trier les données par Temps_Sec
   df_match <- df_match %>%
     arrange(Temps_Sec)
-  
+
   # Extraire code rencontre
   code_renc <- lines[str_detect(lines, "Code Renc")]
   code_renc <- str_extract(code_renc, "Code Renc\\s+(.+)")
@@ -50,7 +50,7 @@ traitement_pdf <- function(pdf_file){   # format "nom_doc.pdf"
   recevant <- str_trim(equipe[[1]][1])
   visiteur <- str_trim(str_extract(equipe[[1]][2], "^[^\\d]+"))
   
-  # Noms coachs
+  # Noms coach
   index_coach <- which(str_detect(lines, "Officiel Resp"))
   
   # Extraire le nom et prénom de la ligne suivante pour chaque occurrence de "Officiel Resp"
@@ -62,7 +62,7 @@ traitement_pdf <- function(pdf_file){   # format "nom_doc.pdf"
   coach_recevant <- coach[1]
   coach_visiteur <- coach[2]
   
-  # Créer le dataframe sur les équipes
+  # tableau avec les infos des équipes
   df_info_equipe <- data.frame(
     code_rencontre = code_renc,
     club_recevant = recevant,
@@ -76,33 +76,29 @@ traitement_pdf <- function(pdf_file){   # format "nom_doc.pdf"
   df_info_match <- cbind(df_info_repeat, df_match)
   rownames(df_info_match) <- NULL
   
+  # Séparer score en score recevant et visiteur
+  df_info_match$score_final <- df_info_match$score[nrow(df_info_match)]
+  df_info_match <- df_info_match %>% separate(col = score,
+                                              into = c("score_recevant", "score_visiteur"),
+                                              sep = '-')
+  df_info_match$score_recevant <- as.numeric(df_info_match$score_recevant)
+  df_info_match$score_visiteur <- as.numeric(df_info_match$score_visiteur)
+  
   return (df_info_match)
 }
 
-traitement_pdf("UAFJKJI.pdf")
-
-
-##### Fonction ajout de variables et tableau final #####
 
 ajout_variables <- function(data_info_match){   # format data.frame
-  df_final <- df_info_match
-  df_final$score_final <- df_info_match$score[nrow(df_info_match)]
-  
-  # Séparer le score en recevant et visiteur
-  df_final <- df_final %>% separate(col = score,
-                                    into = c("score_recevant", "score_visiteur"),
-                                    sep = '-')
-  
   # Extraire les index où chaque équipe à pris un temps mort
   index_tm_visiteur <- which(is.na(str_extract(df_info_match$action, "^Temps Mort.+Visiteur$")) == FALSE)
   index_tm_recevant <- which(is.na(str_extract(df_info_match$action, "^Temps Mort.+Recevant$")) == FALSE)
   
   # Créer une colonne "qui prend le temps mort"
-  df_final$temps_mort_equipe <- ""
-  df_final$temps_mort_equipe[index_tm_recevant] <- "r"
-  df_final$temps_mort_equipe[index_tm_visiteur] <- "v"
+  df_info_match$temps_mort_equipe <- ""
+  df_info_match$temps_mort_equipe[index_tm_recevant] <- "r"
+  df_info_match$temps_mort_equipe[index_tm_visiteur] <- "v"
   
-  # But 1min après par l'équipe qui prend le temps mort oui/non
+  # But 1 min après temps-mort
   df_info_match$but_1min_apres_temps_mort <- ""
   
   for (i in which(df_info_match$temps_mort_equipe != "")){
@@ -126,11 +122,12 @@ ajout_variables <- function(data_info_match){   # format data.frame
     
   }
   
-  # Supprimer colonnes inutiles
+  # Garder les variables qui nous intéressent
   df_final <- df_info_match[grepl("^(Temps Mort|But)", df_info_match$action), ] %>% 
     select(-"action", -"Temps_Sec")
   
   return (df_final)
-}
+}  
+
 
 ajout_variables(traitement_pdf("UAFJKJI.pdf"))
