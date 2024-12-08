85c92d62da711c9a0d70fc5ab2bf680738ab56bd
Marine <lebretonmarine@icloud.com>
2024-12-08 21:29:37 +0100

    nouveau modèle

--- telechargement_classements.R
+++ telechargement_classements.R
@@ -1,11 +1,12 @@
 library(RSelenium)
+library(rvest)
 
 # Lancer un serveur Selenium (assurez-vous que Selenium est installé)
 driver <- rsDriver(browser = "chrome", port = as.integer(4444))
 remote_driver <- driver[["client"]]
 
 # Ouvrir la page web
-remote_driver$navigate("https://www.ffhandball.fr/competitions/saison-2023-2024-19/national/liqui-moly-starligue-2023-24-23266/poule-128388/classements/")
+remote_driver$navigate("https://www.ffhandball.fr/competitions/saison-2022-2023-18/national/proligue-2022-2023-20359/poule-108936/classements/")
 
 # Extraire le code source après chargement de la page
 page_source <- remote_driver$getPageSource()[[1]]
@@ -14,7 +15,7 @@ page_source <- remote_driver$getPageSource()[[1]]
 page <- read_html(page_source)
 
 # Extraire la table
-classement_D1H_2324 <- page %>%
+classement <- page %>%
   html_node("table") %>%
   html_table(fill = TRUE)
 
@@ -22,33 +23,7 @@ classement_D1H_2324 <- page %>%
 remote_driver$close()
 driver$server$stop()
 
-classement_D1H_2324 <- classement_D1H_2324[,-11]
+classement <- classement[,-11]
 
-
-# Lancer un serveur Selenium (assurez-vous que Selenium est installé)
-driver <- rsDriver(browser = "chrome", port = as.integer(4444))
-remote_driver <- driver[["client"]]
-
-# Ouvrir la page web
-remote_driver$navigate("https://www.ffhandball.fr/competitions/saison-2022-2023-18/national/liqui-moly-starligue-2022-23-20342/poule-108997/classements/")
-
-# Extraire le code source après chargement de la page
-page_source <- remote_driver$getPageSource()[[1]]
-
-# Charger le contenu avec rvest
-page <- read_html(page_source)
-
-# Extraire la table
-classement_D1H_2223 <- page %>%
-  html_node("table") %>%
-  html_table(fill = TRUE)
-
-# Fermer le navigateur
-remote_driver$close()
-driver$server$stop()
-
-classement_D1H_2223 <- classement_D1H_2223[,-11]
-
-write.csv(classement_D1H_2223, "classement_D1H_2223.csv", row.names = FALSE)
-write.csv(classement_D1H_2324, "classement_D1H_2324.csv", row.names = FALSE)
+write.csv(classement, "classement_D2H_2223.csv", row.names = FALSE)
 
