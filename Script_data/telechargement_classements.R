e22eb80a5619dab25ae67118aeab78f4d33ce17b
Ameliee12 <amelie.brejot@wanadoo.fr>
2024-12-17 12:27:00 +0100

    organisation dossier

--- Script_data/telechargement_classements.R
+++ Script_data/telechargement_classements.R
@@ -6,7 +6,7 @@ driver <- rsDriver(browser = "chrome", port = as.integer(4444))
 remote_driver <- driver[["client"]]
 
 # Ouvrir la page web
-remote_driver$navigate("https://www.ffhandball.fr/competitions/saison-2022-2023-18/national/proligue-2022-2023-20359/poule-108936/classements/")
+remote_driver$navigate("https://www.ffhandball.fr/competitions/saison-2021-2022-17/national/proligue-2021-2022-17871/poule-90408/classements/")
 
 # Extraire le code source après chargement de la page
 page_source <- remote_driver$getPageSource()[[1]]
@@ -25,5 +25,5 @@ driver$server$stop()
 
 classement <- classement[,-11]
 
-write.csv(classement, "classement_D2H_2223.csv", row.names = FALSE)
+write.csv(classement, "classement_D2H_2122.csv", row.names = FALSE)
 
